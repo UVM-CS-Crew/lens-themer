126f57b6d212178a7f851d3f9df1b8653324146f
John Urbani <35612888+JohnUrbani@users.noreply.github.com>
2020-10-10 12:51:53 -0400

    Halloween separated, add js menu

--- content_script.js
+++ content_script.js
@@ -1,27 +1,21 @@
-//var element = document.querySelector('main');
-//element.parentElement.removeChild(element);
 
-var minBackgrounds = 1;
-var maxBackgrounds = 5;
 
-var temp = document.createElement('div');
+var mode = "halloween";
 
-temp.innerHTML = "<style>@import url('https://fonts.googleapis.com/css2?family=Sedgwick+Ave+Display&display=swap'); body { background-image: url('".concat(chrome.runtime.getURL("images/".concat((Math.floor(((Math.random() * (maxBackgrounds - minBackgrounds)) + minBackgrounds))).toString()).concat(".png') !important;}</style>")));
+var element = document.querySelector("body");
+element.classList.add(mode);
 
-var head = document.head;
+if (mode === "halloween") {
+	var minBackgrounds = 1;
+	var maxBackgrounds = 5;
+	
+	var temp = document.createElement('div');
 
-while (temp.firstChild) {
-    head.appendChild(temp.firstChild);
+	temp.innerHTML = "<style>@import url('https://fonts.googleapis.com/css2?family=Sedgwick+Ave+Display&display=swap'); body { background-image: url('".concat(chrome.runtime.getURL("images/".concat((Math.floor(((Math.random() * (maxBackgrounds - minBackgrounds)) + minBackgrounds))).toString()).concat(".png') !important;}</style>")));
+
+	var head = document.head;
+
+	while (temp.firstChild) {
+		head.appendChild(temp.firstChild);
+	}
 }
-
-
-
-/*var temp = document.createElement('div');
-
-temp.innerHTML = "<style></style>");
-
-var head = document.head;
-
-while (temp.firstChild) {
-    head.appendChild(temp.firstChild);
-}*/
