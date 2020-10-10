b989a5afba563e87dd0381394b6dd158bcf318a3
John Urbani <35612888+JohnUrbani@users.noreply.github.com>
2020-10-10 12:08:30 -0400

    Halloween theming working on most websites

--- content_script.js
+++ content_script.js
@@ -1,2 +1,27 @@
-var element = document.querySelector('div#mw-panel');
-element.parentElement.removeChild(element);
+//var element = document.querySelector('main');
+//element.parentElement.removeChild(element);
+
+var minBackgrounds = 1;
+var maxBackgrounds = 5;
+
+var temp = document.createElement('div');
+
+temp.innerHTML = "<style>@import url('https://fonts.googleapis.com/css2?family=Sedgwick+Ave+Display&display=swap'); body { background-image: url('".concat(chrome.runtime.getURL("images/".concat((Math.floor(((Math.random() * (maxBackgrounds - minBackgrounds)) + minBackgrounds))).toString()).concat(".png') !important;}</style>")));
+
+var head = document.head;
+
+while (temp.firstChild) {
+    head.appendChild(temp.firstChild);
+}
+
+
+
+/*var temp = document.createElement('div');
+
+temp.innerHTML = "<style></style>");
+
+var head = document.head;
+
+while (temp.firstChild) {
+    head.appendChild(temp.firstChild);
+}*/
